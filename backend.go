2c1043c00f2035322499d148f5406f3dd0fc41af
K.S.Adithya <adikarks@gmail.com>
2026-07-10 21:56:04 +0530

    Addded Graceful Shutdown using Channels and OS library

--- backend.go
+++ backend.go
@@ -4,15 +4,27 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"sync"
+	"time"
+)
+
+type CircuitState int
+
+const (
+	Closed CircuitState = iota
+	Open
+	HalfOpen
 )
 
 type Backend struct {
-	URL          *url.URL
-	Weight       int
-	Alive        bool
-	mux          sync.RWMutex
-	reverseproxy *httputil.ReverseProxy
-	Stats        MonitoringStats
+	URL             *url.URL
+	Weight          int
+	Alive           bool
+	mux             sync.RWMutex
+	reverseproxy    *httputil.ReverseProxy
+	Stats           MonitoringStats
+	FailureCount    int
+	CircuitState    CircuitState
+	LastFailureTime time.Time
 }
 
 func (b *Backend) setAlive(alive bool) {
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -70,17 +74,47 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", lb)
 	mux.Handle("/metrics", promhttp.Handler())
+	mux.HandleFunc("/backends", BackendsHandler)
 
 	server := http.Server{
 		Addr:    fmt.Sprintf(":%d", port),
 		Handler: mux,
 	}
 
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(
+		quit,
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+
 	go healthCheck(healthInterval)
 
 	log.Printf("Load Balancer started at :%d\n", port)
 
-	if err := server.ListenAndServe(); err != nil {
+	go func() {
+
+		if err := server.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
+
+			log.Fatal(err)
+
+		}
+
+	}()
+
+	<-quit
+	log.Println("Shutting down server....")
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		30*time.Second,
+	)
+
+	defer cancel()
+	if err := server.Shutdown(ctx); err != nil {
 		log.Fatal(err)
 	}
+
+	log.Println("Server exited gracefully.")
 }
